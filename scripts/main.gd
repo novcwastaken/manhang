6054fca83de692c71f4cf7dd4155603948592106
novcwastaken <rob.kosapeter@gmail.com>
2026-04-06 22:11:12 +0200

    Added pause menu & updated main menu
    Added my name to the title of the main menu, and added credits

--- scripts/main.gd
+++ scripts/main.gd
@@ -62,7 +62,6 @@ const WIN_PICTURE: String = '''
 
 const VALID_LETTERS: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var WORDS: Array[String]
-#const WORDS: Array[String] = ["UKRAJNA", "HABORU", "ZELENSZKIJ", "BRUSSZEL", "BENZIN", "MIGRANS"]
 
 const LETTER_BUFFER_BB_PREFIX: String = "[pulse freq=2.0 color=#ffffff00 ease=-1000.0]"
 
@@ -73,7 +72,11 @@ var missed_letters: Array[String]
 var guess_input_enabled: bool = true
 var won: bool = false
 
+var is_paused: bool = false
+
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
 	load_words()
 
 	current_word = WORDS[randi_range(0, WORDS.size() - 1)]
@@ -81,8 +84,14 @@ func _ready() -> void:
 
 	$GameOverScreen/MainScreen/RantLabel.text = "A critical failure has occurred (you). I'm not collecting any error info, fuck you. What am I to you, a charity? Just get better at the game and don't flail around guessing letters like a coked up chimpanzee. I'd have more faith in a lobotomized sunfish than you to figure out that the word was %s. Sincerely, eat shit." % current_word.to_upper()
 
-	$WordLabel.text = "_".repeat(len(current_word))
-	$ArtLabel.text = PICTURES[0]
+	$GameThings/WordLabel.text = "_".repeat(len(current_word))
+	$GameThings/ArtLabel.text = PICTURES[0]
+
+	$GameThings.visible = true
+	$BufferThings.visible = false
+	$GameOverScreen.visible = false
+	$WinThings.visible = false
+	$PauseMenu.visible = false
 
 	update_buffer_displays()
 	update_misses_displays()
@@ -95,14 +104,14 @@ func _input(event: InputEvent) -> void:
 
 			if input in missed_letters:
 				play_sfx_with_random_pitch($SFX/BufferAlreadyGuessed)
-				$MissesLabel.label_settings.font_color = Color(255, 255, 0)
+				$GameThings/MissesLabel.label_settings.font_color = Color(255, 255, 0)
 				await get_tree().create_timer(0.1).timeout
-				$MissesLabel.label_settings.font_color = Color(255, 0, 0)
-			elif input in $WordLabel.text:
+				$GameThings/MissesLabel.label_settings.font_color = Color(255, 0, 0)
+			elif input in $GameThings/WordLabel.text:
 				play_sfx_with_random_pitch($SFX/BufferAlreadyGuessed)
-				$WordLabel.label_settings.font_color = Color(255, 255, 0)
+				$GameThings/WordLabel.label_settings.font_color = Color(255, 255, 0)
 				await get_tree().create_timer(0.1).timeout
-				$WordLabel.label_settings.font_color = Color(255, 255, 255)
+				$GameThings/WordLabel.label_settings.font_color = Color(255, 255, 255)
 			else:
 				letter_buffer = input
 				update_buffer_displays()
@@ -129,6 +138,17 @@ func _input(event: InputEvent) -> void:
 		if Input.is_action_just_pressed("escape"):
 			get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
+	# Pause
+	if Input.is_action_just_pressed("escape") && !$GameOverScreen/MainScreen.visible && !won:
+		is_paused = true
+		guess_input_enabled = false
+		
+		$GameThings.visible = false
+		$BufferThings.visible = false
+
+		$PauseMenu.visible = true
+		$PauseMenu/MenuItemPanel/VBoxContainer.get_child(0).grab_focus()
+
 func letter_input_is_valid(event: InputEvent) -> bool:
 	if event is not InputEventKey: return false
 	if event.echo: return false
@@ -142,9 +162,9 @@ func guess_letter(letter: String) -> bool:
 	for i in len(current_word):
 		var character: String = current_word[i]
 		if character == letter:
-			$WordLabel.text[i] = character
+			$GameThings/WordLabel.text[i] = character
 			is_match = true
-			if "_" in $WordLabel.text: play_sfx_with_random_pitch($SFX/BufferHit)
+			if "_" in $GameThings/WordLabel.text: play_sfx_with_random_pitch($SFX/BufferHit)
 
 	if !is_match && letter not in missed_letters:
 		missed_letters.append(letter)
@@ -153,7 +173,7 @@ func guess_letter(letter: String) -> bool:
 		$AnimationPlayer.play("miss_flash")
 		if missed_letters.size() < PICTURES.size(): play_sfx_with_random_pitch($SFX/BufferMiss)
 
-	if $WordLabel.text.to_upper() == current_word.to_upper():
+	if $GameThings/WordLabel.text.to_upper() == current_word.to_upper():
 		won = true
 		game_won()
 		play_sfx_with_random_pitch($SFX/Win)
@@ -164,15 +184,15 @@ func update_buffer_displays() -> void:
 	if letter_buffer == "":
 		$BufferThings/BufferLabel.text = letter_buffer
 		$BufferThings.visible = false
-		if !won: $BufferLetterHint.visible = true
+		if !won: $GameThings/BufferLetterHint.visible = true
 	else:
 		$BufferThings/BufferLabel.text = LETTER_BUFFER_BB_PREFIX + letter_buffer
 		$BufferThings.visible = true
-		$BufferLetterHint.visible = false
+		$GameThings/BufferLetterHint.visible = false
 
 func update_misses_displays() -> void:
-	$MissesLabel.text = ", ".join(missed_letters)
-	if missed_letters.size() <= PICTURES.size() - 1 && missed_letters.size() > 0: $ArtLabel.text = PICTURES[missed_letters.size()]
+	$GameThings/MissesLabel.text = ", ".join(missed_letters)
+	if missed_letters.size() <= PICTURES.size() - 1 && missed_letters.size() > 0: $GameThings/ArtLabel.text = PICTURES[missed_letters.size()]
 
 func game_over() -> void:
 	guess_input_enabled = false
@@ -183,8 +203,8 @@ func game_over() -> void:
 
 func game_won() -> void:
 	guess_input_enabled = false
-	$ArtLabel.text = WIN_PICTURE
-	$BufferLetterHint.visible = false
+	$GameThings/ArtLabel.text = WIN_PICTURE
+	$GameThings/BufferLetterHint.visible = false
 	$BufferThings.visible = false
 	$WinThings.visible = true
 
@@ -198,3 +218,32 @@ func load_words() -> void:
 
 	for word in content.split(","):
 		WORDS.append(word.strip_edges().to_upper())
+
+
+func _on_pause_continue_button_pressed() -> void:
+	is_paused = false
+	guess_input_enabled = true
+
+	$GameThings.visible = true
+
+	var is_letter_buffer_empty: bool = letter_buffer == ""
+
+	$BufferThings.visible = !is_letter_buffer_empty
+	$GameThings/BufferLetterHint.visible = is_letter_buffer_empty
+
+	$PauseMenu.visible = false
+
+func _on_pause_new_game_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_pause_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_pause_continue_button_focus_exited() -> void:
+	play_sfx_with_random_pitch($SFX/GameOverMainScreen)
+
+func _on_pause_new_game_button_focus_exited() -> void:
+	play_sfx_with_random_pitch($SFX/GameOverMainScreen)
+
+func _on_pause_main_menu_button_focus_exited() -> void:
+	play_sfx_with_random_pitch($SFX/GameOverMainScreen)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,12 +2,21 @@ extends Control
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	$Background/MenuItemPanel/VBoxContainer.get_child(0).grab_focus()
+	_on_credits_back_button_pressed() # yes, I just did that
+	#$Background/MenuItemPanel/Main.get_child(0).grab_focus()
 
+func _on_manhang_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
+func _on_credits_button_pressed() -> void:
+	$Background/MenuItemPanel/Credits.visible = true
+	$Background/MenuItemPanel/Main.visible = false
+	$Background/MenuItemPanel/Credits.get_child(0).grab_focus()
 
-func _on_manhang_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+func _on_credits_back_button_pressed() -> void:
+	$Background/MenuItemPanel/Credits.visible = false
+	$Background/MenuItemPanel/Main.visible = true
+	$Background/MenuItemPanel/Main.get_child(0).grab_focus()
